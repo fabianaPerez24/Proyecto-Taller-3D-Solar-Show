5944f609264f0010dd8e16a2e08c3a1fd5ad6c18
fabianaPerez24 <fabianalertora003@gmail.com>
2026-07-09 12:32:29 -0500

    Se reemplazaron scripts y la escena

--- Assets/Scenes/Multiplayer/Scripts/DisableByTag.cs
+++ Assets/Scenes/Multiplayer/Scripts/DisableByTag.cs
@@ -5,9 +5,9 @@ public class DeactivateByTag : MonoBehaviour
     public void DeactivateObject(string tag)
     {
         GameObject target = GameObject.FindGameObjectWithTag(tag);
-        if (target != null)
-        {
-            target.SetActive(false);
-        }
+        //if (target != null)
+        //{
+        //    target.SetActive(false);
+        //}
     }
 }
